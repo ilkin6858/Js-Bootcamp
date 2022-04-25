f1781d6f7a4df0425cedd3b0bcc89b09f3829ae8
ilkin6858 <ilkin6858@gmail.com>
2022-04-25 13:21:16 +0400

    learning DOM

--- DAY05/DOM/app.js
+++ DAY05/DOM/app.js
@@ -26,3 +26,60 @@ element.innerText="Coding is my life"
 element.innerHTML="<h5>merhaba</h5>";
 
 document.getElementById("box").style.backgroundColor='aqua';
+
+//////////////////////
+
+//getElementsByclassName()
+
+//elementlere class adlari ile muraciet elemek ede bilirik,class adlari cox olduqu ucun elements kimi yazilir.
+// class adlarina arraylerle muraciet elemeyimin sebebi ise class adlari cox olduqu ucundu
+
+let classname=document.getElementsByClassName('row');
+console.log(classname[0]);
+classname[1].textContent='qasimov';
+
+document.getElementsByClassName('row')[1].style.borderRight='10px solid green'
+
+
+
+
+//document.getElementsByTagname.
+// class adlarindan coxda ferqi yoxdu, sadece olaraq taq adlarina muraciet edirik
+
+console.log(document.getElementsByTagName('h1'));
+document.getElementsByTagName('h1')[4].textContent='yasasin azerbaycan';
+document.getElementsByTagName('h1')[4].style.color='green';
+document.getElementsByTagName('h1')[4].style.fontSize='3em';
+document.getElementsByTagName('h1')[4].style.border='5px solid red';
+document.getElementsByTagName('h1')[4].style.borderRadius='20px';
+document.getElementsByTagName('h1')[4].style.textAlign='center';
+
+let mytag=document.getElementsByTagName('h1')
+
+//bu yazdiqim kodlarin butun teglere bir anda elave olunmasini isdeyirem looplardan istifade elemeliyem;
+
+for(let i=0; i<mytag.length; i++){
+
+    mytag[i].textContent='yasasin azerbaycan';
+    mytag[i].style.backgroundColor="blue";
+    mytag[i].style.textAlign='center';
+    mytag[i].style.border='5px solid black'
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
